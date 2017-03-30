99bf0f998139f622c711b7e335d335dfc29210c2
John Fish <john@johnafish.ca>
2017-03-29 20:34:40 -0400

    Got works working, basic trout functionality

--- works/trout.py
+++ works/trout.py
@@ -2,29 +2,44 @@ import markdown, os
 
 path = os.getcwd()
 
+headerOne = open("headerOne", "r").read()
+headerTwo = open("headerTwo", "r").read()
+footer = open("footer", "r").read()
+indexFile = open("index.html", "w")
+
 def nameFormat(name):
 	return name
 
 def writeFile(out, name, content):
-	out.write(header1)
+	out.write(headerOne)
 	out.write(nameFormat(name))
-	out.write(header2)
+	out.write(headerTwo)
 	out.write(markdown.markdown(content))
 	out.write(footer)
 
+
+
 def indexEntry(fileName):
-	return True
+	indexFile.write("<li class='work'><a href='"+fileName+".html'>"+nameFormat(fileName)+"</a></li>")
+
 
 def writeFiles():
+	indexFile.write(headerOne)
+	indexFile.write(nameFormat("works"))
+	indexFile.write(headerTwo)
+	indexFile.write("<ul class='workList'>")
+
 	for file in os.listdir("."):
-		if(file[-3:]=".md"):
+		if(file[-3:]==".md"):
 			fileName = file[:-3]
 			out=open(fileName+".html", "w")
-			writeFile(out, fileName, file.read())
+			writeFile(out, fileName, open(file, "r").read())
 			indexEntry(fileName)
+			out.close()
 
+	indexFile.write("</ul>")
+	indexFile.write(footer)
+	indexFile.close()
 
-file = open('test.md', 'r')
-out = open("test.html", "w")
-
-out.write(markdown.markdown(file.read()))
+if (__name__ == "__main__"):
+	writeFiles()
