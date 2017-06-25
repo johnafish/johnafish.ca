e5389437567858e16b319898a10420b8a21f5e5b
John Fish <john@johnafish.ca>
2017-06-25 16:06:09 -0400

    Updated for use with youtube/more general

--- automate/app.js
+++ automate/app.js
@@ -2,31 +2,51 @@ var database = firebase.database();
 var followersReference = firebase.database().ref('followers');
 var statusReference = firebase.database().ref('status');
 var followerTrendReference = firebase.database().ref('followerTrend');
+var subscribersReference = firebase.database().ref('subscribers');
+var subscriberTrendReference = firebase.database().ref('subscriberTrend');
 var data;
-var chart;
-var drawn = false;
-var options;
+var charts={};
+var drawn = 0;
+var total = 2;
+var options = {
+				'titleposition': 'none',
+				'width':800,
+				'height':600,
+				'legend':{
+					position: 'none',
+				},
+				'curveType': 'function',
+				'animation':{
+					duration: 1000,
+					easing: 'out',
+				}
+			};
 var currentData={};
+var currentSubscribers = 0;
+var currentFollowers = 0;
+var instaGraphName = "instaFollowers"
+var ytGraphName = "subscribers"
 
-function updateFollowerCount(value){
-	$("#followers").text(value["count"]);
+function updateInstaFollowerCount(value){
+	$("#instaFollowers").text(value["count"]);
+	currentFollowers=value["count"];
+}
+
+function updateSubscribeCount(value){
+	$("#subscribers").text(value["count"]);
+	currentSubscribers=value["count"];
 }
 
 function updateStatus(value){
 	$("#status").text(value["message"]);
 }
 
-function paintChart(){
-	followerTrendReference.once('value').then(function(snapshot) {
-		drawChart(snapshot.val())
-	});
-}
-
-function updateChart(value){
-	if(drawn){
+function updateChart(value,name){
+	if(drawn>=total){
 		data= new google.visualization.DataTable();
 		data.addColumn('date', 'Time');
 		data.addColumn('number', 'Followers');
+		console.log(value)
 		for(var key in value){
 			var sK = value[key]["date"].split(" ");
 			var date = new Date(sK[0],sK[1]-1,sK[2],sK[3],sK[4],sK[5]);
@@ -34,12 +54,15 @@ function updateChart(value){
 			data.addRow([date,numFollowers]);
 		}
 		data.sort({column:0, desc:true})
-		chart.draw(data, options);
+		console.log(options)
+		charts[name].draw(data, options);
 		currentData=JSON.parse(JSON.stringify(value));
 	}
 }
 
-function drawChart(value){
+function drawChart(value, name, elementId){
+	console.log(name)
+	console.log(value)
 	currentData = JSON.parse(JSON.stringify(value));
 	data = new google.visualization.DataTable();
 	data.addColumn('date', 'Time');
@@ -52,25 +75,27 @@ function drawChart(value){
 		data.addRow([date,numFollowers]);
 	}
 	data.sort({column:0, desc:true})
-	// Set chart options
-	options = {'title':'Follower Count Over Time',
-					'width':800,
-					'height':600,
-					'animation':{
-						duration: 1000,
-						easing: 'out',
-					}
-				};
+	charts[name] = new google.visualization.LineChart(document.getElementById(elementId));
+	charts[name].draw(data, options);
+	drawn++;
+}
 
-	// Instantiate and draw our chart, passing in some options.
-	chart = new google.visualization.LineChart(document.getElementById('graph'));
-	chart.draw(data, options);
-	drawn = true;
+function paintCharts(){
+	subscriberTrendReference.once('value').then(function(snapshot) {
+		drawChart(snapshot.val(),ytGraphName,"graphSubscribers")
+	});
+	followerTrendReference.once('value').then(function(snapshot) {
+		drawChart(snapshot.val(),instaGraphName,"graphInstaFollowers")
+	});
 }
 
 $(function(){
 	followersReference.once('value').then(function(snapshot) {
-		updateFollowerCount(snapshot.val());
+		updateInstaFollowerCount(snapshot.val());
+	});
+
+	subscribersReference.once('value').then(function(snapshot) {
+		updateSubscribeCount(snapshot.val());
 	});
 
 	statusReference.once('value').then(function(snapshot) {
@@ -78,7 +103,7 @@ $(function(){
 	});
 	
 	google.charts.load('current', {'packages':['corechart']});
-	google.charts.setOnLoadCallback(paintChart());
+	google.charts.setOnLoadCallback(paintCharts());
 });
 
 statusReference.on('value', function(snapshot) {
@@ -86,9 +111,17 @@ statusReference.on('value', function(snapshot) {
 });
 
 followersReference.on('value', function(snapshot) {
-  updateFollowerCount(snapshot.val());
+  updateInstaFollowerCount(snapshot.val());
 });
 
 followerTrendReference.on('value', function(snapshot) {
-  updateChart(snapshot.val());
+  updateChart(snapshot.val(),instaGraphName);
+});
+
+subscribersReference.on('value', function(snapshot){
+	updateSubscribeCount(snapshot.val());
+});
+
+subscriberTrendReference.on('value', function(snapshot) {
+  updateChart(snapshot.val(),ytGraphName);
 });
